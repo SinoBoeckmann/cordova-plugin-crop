cad85470a9c1d0501f7e2817fe5b4b67b76d8d8e
Sino <SinoBoeckmann@users.noreply.github.com>
2017-11-14 17:02:57 +0100

    Update CropPlugin.java
    Testing https://github.com/obeza/cordova-plugin-crop-with-ratio/blob/master/src/android/CropPlugin.java

--- src/android/CropPlugin.java
+++ src/android/CropPlugin.java
@@ -24,38 +24,55 @@ public class CropPlugin extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
-        if (action.equals("cropImage")) {
-            String imagePath = args.getString(0);
-            JSONObject options = args.getJSONObject(1);
-            int targetWidth = options.getInt("targetWidth");
-            int targetHeight = options.getInt("targetHeight");
-            int isProfile = options.getInt("isProfile");
+      if (action.equals("cropImage")) {
+          //String imagePath = args.getString(0);
+          String imagePath = args.getString(0);
+          JSONObject options = args.getJSONObject(1);
 
-            this.inputUri = Uri.parse(imagePath);
-            this.outputUri = Uri.fromFile(new File(getTempDirectoryPath() + "/" + System.currentTimeMillis()+ "-cropped.jpg"));
+          //this.inputUri = Uri.parse(imagePath);
+          //this.outputUri = Uri.fromFile(new File(getTempDirectoryPath() + "/" + System.currentTimeMillis()+ "-cropped.jpg"));
+          int widthRatio = options.getInt("widthRatio");
+          int heightRatio = options.getInt("heightRatio");
+          int targetWidth = options.getInt("targetWidth");
+          int targetHeight = options.getInt("targetHeight");
 
-            PluginResult pr = new PluginResult(PluginResult.Status.NO_RESULT);
-            pr.setKeepCallback(true);
-            callbackContext.sendPluginResult(pr);
-            this.callbackContext = callbackContext;
+          //PluginResult pr = new PluginResult(PluginResult.Status.NO_RESULT);
+          //pr.setKeepCallback(true);
+          //callbackContext.sendPluginResult(pr);
+          //this.callbackContext = callbackContext;
 
-            cordova.setActivityResultCallback(this);
-            Crop crop = Crop.of(this.inputUri, this.outputUri);
-            if(targetHeight != -1 && targetWidth != -1 ) {
-                crop.withMaxSize(targetWidth, targetHeight);
-                if(targetWidth == targetHeight) {
-                    crop.asSquare();
-                }
-            }
+          this.inputUri = Uri.parse(imagePath);
+          this.outputUri = Uri.fromFile(new File(getTempDirectoryPath() + "/" + System.currentTimeMillis()+ "-cropped.jpg"));
 
-            if(isProfile == 1){
-                crop.asSquare();
-            }
 
-            crop.start(cordova.getActivity());
-            return true;
-        }
-        return false;
+/*           cordova.setActivityResultCallback(this);
+          Crop.of(this.inputUri, this.outputUri)
+                  .asSquare()
+                  .start(cordova.getActivity());
+          return true; */
+
+        PluginResult pr = new PluginResult(PluginResult.Status.NO_RESULT);
+          pr.setKeepCallback(true);
+          callbackContext.sendPluginResult(pr);
+          this.callbackContext = callbackContext;
+  
+          cordova.setActivityResultCallback(this);
+          Crop croper = Crop.of(this.inputUri, this.outputUri);
+  
+          if(targetHeight > 0 && targetWidth > 0) {
+              croper.withMaxSize(targetWidth, targetHeight);
+          }
+            
+          if(widthRatio > 0 && heightRatio > 0){
+              croper.withAspect(widthRatio, heightRatio);
+          } 
+  
+          croper.start(cordova.getActivity());
+  
+          return true;
+
+      }
+      return false;
     }
 
     @Override
@@ -135,3 +152,8 @@ public class CropPlugin extends CordovaPlugin {
         this.callbackContext = callbackContext;
     }
 }
+Viewport
+Window
+×
+×
+×
